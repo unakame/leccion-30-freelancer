bf28fe7886a9a49abf579c8117e318e00aac9509
Dana Franco <turquoise.ilg@hotmail.com>
2017-05-10 14:35:58 -0500

    modals

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-/*
- * Archivo principal de JS
- */
+
+var cabin = document.getElementById("cabin");
+var modal1 = document.getElementById("modal1");
+var cabinMod = document.getElementById("cabinModal");
+var captionText = document.getElementById("caption");
+
+cabin.onclick = function(){
+    modal1.style.display = "block";
+    cabinMod.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal1.style.display = "none";
+}
